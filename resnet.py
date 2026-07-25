e354e0f6d69cc93b3cf84b4227c81cd4eb02353d
emdoubleu <me@emdoubl.eu>
2026-07-25 15:11:58 +0200

    Update model to accept grayscale images

--- resnet.py
+++ resnet.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from torchvision import models
 
@@ -6,6 +7,29 @@ from config import NUM_CLASSES
 model = models.resnet18(
     weights = models.ResNet18_Weights.DEFAULT
 )
+
+# Replace 3D (RGB) convolution layer to 1D (grayscale)
+conv = nn.Conv2d(
+    in_channels  = 1,
+    out_channels = model.conv1.out_channels,
+    kernel_size  = model.conv1.kernel_size,         # pyright: ignore[reportArgumentType]
+    stride       = model.conv1.stride,              # pyright: ignore[reportArgumentType]
+    padding      = model.conv1.padding,             # pyright: ignore[reportArgumentType]
+    dilation     = model.conv1.dilation,            # pyright: ignore[reportArgumentType]
+    groups       = model.conv1.groups,
+    bias         = (model.conv1.bias is not None),
+    padding_mode = model.conv1.padding_mode
+)
+
+with torch.no_grad():
+    conv.weight = nn.Parameter(
+        torch.sum(
+            model.conv1.weight.clone(),
+            dim = 1, keepdim = True
+        )
+    )
+
+model.conv1 = conv
 
 # Freeze all layers to keep basic feature detection intact
 for name, param in model.named_parameters():
