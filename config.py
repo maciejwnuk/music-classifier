c04c8e22c211073c855ca321fdd102ae180f1485
emdoubleu <me@emdoubl.eu>
2026-07-26 15:05:34 +0200

    Change FFT_HOP parameter to increase overlap

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DYNAMIC_RANGE    = 70                  # Range of dB to cover
 SAMPLE_RATE      = 22050               # Audio target sample rate
 SEGMENT_DURATION = 5.0                 # Audio segment duration in seconds
 FFT_POINTS       = 512                 # FFT resolution
-FFT_HOP          = 492                 # FFT hop size
+FFT_HOP          = 256                 # FFT hop size
 IMG_SIZE         = (224, 224)          # ResNet Input size
 
 BATCH_SIZE    = 32                     # Batch size
